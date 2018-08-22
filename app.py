985676d32c61e26e6b1cf4aeedc73aa0e5528a84
Nikolay <kombarov55@gmail.com>
2018-08-22 22:19:39 +0300

    changes

--- app.py
+++ app.py
@@ -162,6 +162,7 @@ def optionsToKeyboard(options):
     buttons = list(map(lambda option: [{"color": "default", "action": {"type": "text", "label": option["text"]}}], options))
     keyboard = {"one_time": True, "buttons": buttons}
     keyboard = json.dumps(keyboard)
+    keyboard.encode("utf-8")
     return keyboard
 
 def sendKeyboardMessage(userId, text, options):
