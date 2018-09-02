f744c27173e7df3c57bdd6620764c5cd2fbc0501
na_kombarov <kombarov55@gmail.com>
2018-09-02 14:49:09 +0300

    changes

--- ApiGate.py
+++ ApiGate.py
@@ -13,7 +13,7 @@ def sendTextMessage(userId, text):
 def optionsToKeyboard(options):
     buttons = list(map(lambda option: [{"color": "default", "action": {"type": "text", "label": option["text"]}}], options))
     keyboard = {"one_time": True, "buttons": buttons}
-    keyboard = json.dumps(keyboard)
+    keyboard = json.dumps(keyboard, ensure_ascii = False)
     return keyboard
 
 def sendKeyboardMessage(userId, text, options):
