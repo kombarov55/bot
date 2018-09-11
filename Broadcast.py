82b285442cb4d0b3eb7e349268fd22181ae8fb0f
na_kombarov <kombarov55@gmail.com>
2018-09-12 02:32:39 +0300

    changes

--- Broadcast.py
+++ Broadcast.py
@@ -18,7 +18,7 @@ def unsubscribe(userId):
     print("Broadcast: unsubscribed user with userId=" + str(userId))
     db[userId] = False
 
-def sendLoop():
+def start():
     print("send loop")
     scheduler = BackgroundScheduler()
     scheduler.start()
@@ -30,14 +30,11 @@ def sendLoop():
         replace_existing = True
     )
     atexit.register(lambda: scheduler.shutdown())
-    
-    #Timer(5.0, sendLoop).start() 
-    
-    #for userId, isSubscribed in db.items(): 
-        #if isSubscribed:
-            #msg = Predictions.getPrediction(userId)
-            #print("send prediction to " + userId ": " + msg)
-            #ApiGate.sendTextMessage(userId, msg)
 
-def p():
-    print("OK")
+def broadcast():
+    for userId, isSubscribed in db.items(): 
+        if isSubscribed:
+            msg = Predictions.getPrediction(userId)
+            print("send prediction to " + userId ": " + msg)
+            ApiGate.sendTextMessage(userId, msg)
+            
